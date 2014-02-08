c36659746149b6e24c2036c61843612f1c514880
Per Liedman <per@liedman.net>
2014-02-08 21:20:28 +0100

    Fixed missing map

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var map = L.map('map');
+
 L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.map-mmgw7jk5/{z}/{x}/{y}.png', {
 	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
 		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
