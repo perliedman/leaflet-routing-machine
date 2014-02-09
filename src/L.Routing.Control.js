d736eca8186a2bcd95bdc995916f1a5b03e56eee
Per Liedman <per@liedman.net>
2014-02-09 21:58:14 +0100

    Initial geocoding support

--- src/L.Routing.Control.js
+++ src/L.Routing.Control.js
@@ -21,8 +21,14 @@
 		},
 
 		onAdd: function(map) {
+			var container = L.Routing.Itinerary.prototype.onAdd.call(this, map);
+
 			this._map = map;
-			return L.Routing.Itinerary.prototype.onAdd.call(this, map);
+			if (this.options.geocoder) {
+				container.insertBefore(this._createGeocoders(), container.firstChild);
+			}
+
+			return container;
 		},
 
 		onRemove: function(map) {
@@ -32,12 +38,12 @@
 			return L.Routing.Itinerary.prototype.onRemove.call(this, map);
 		},
 
-		setVias: function(waypoints) {
+		setWaypoints: function(waypoints) {
 			this._waypoints = waypoints;
 			this._router.route(waypoints);
 		},
 
-		spliceVias: function() {
+		spliceWaypoints: function() {
 			var removed = [].splice.apply(this._waypoints, arguments);
 			this._router.route(this._waypoints);
 			return removed;
@@ -52,9 +58,13 @@
 
 			this._line = L.Routing.line(route);
 			this._line.addTo(this._map);
+			this._hookEvents(this._line);
 			this._map.fitBounds(this._line.getBounds());
 
-			this._hookEvents(this._line);
+			if (this.options.geocoder) {
+				this._setGeocoderValue(0, route.summary.start_point);
+				this._setGeocoderValue(this._geocoderElems.length - 1, route.summary.end_point);
+			}
 		},
 
 		_hookEvents: function(l) {
@@ -73,8 +83,48 @@
 			});
 
 			l.on('waypointadded', function(e) {
-				this.spliceVias(e.afterIndex + 1, 0, e.latlng);
+				this.spliceWaypoints(e.afterIndex + 1, 0, e.latlng);
 			}, this);
+		},
+
+		_createGeocoders: function() {
+			var container = L.DomUtil.create('div', 'leaflet-routing-geocoders'),
+			    i,
+			    geocoder,
+			    listener;
+
+			this._geocoderElems = [];
+
+			for (i = 0; i < Math.max(this._waypoints.length, 2); i++) {
+				geocoder = L.DomUtil.create('input', '', container);
+				if (i === 0) {
+					geocoder.placeholder = 'Start';
+				} else if (i >= this._waypoints.length - 1) {
+					geocoder.placeholder = 'End';
+				} else {
+					geocoder.placeholder = 'Via';
+				}
+
+				listener = this._createGeocodeListener(i);
+				L.DomEvent.addListener(geocoder, 'keydown', listener, this);
+				this._geocoderElems.push(geocoder);
+			}
+
+			return container;
+		},
+
+		_createGeocodeListener: function(i) {
+			return function(e) {
+				if (e.keyCode === 13) {
+					this.options.geocoder.geocode(e.target.value, function(results) {
+						this.spliceWaypoints(i, 1, results[0].center);
+					}, this);
+				}
+			};
+		},
+
+		_setGeocoderValue: function(i, v) {
+			this._geocoderElems[i].value = this._geocoderElems[i].value || v;
 		}
 	});
 
