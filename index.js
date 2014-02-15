98e1f1ace1c2ac5ed3b2c4d7c2aa49e4ed626c36
Per Liedman <per@liedman.net>
2014-02-15 11:18:20 +0100

    Use new mapbox url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var map = L.map('map');
 
-L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.map-mmgw7jk5/{z}/{x}/{y}.png', {
+L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png', {
 	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
 		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
 		'data uses <a href="http://opendatacommons.org/licenses/odbl/">ODbL</a> license'
