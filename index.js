d13181ad73accf6b87a2472a850b643b22ce72ac
Per Liedman <per@liedman.net>
2014-02-24 14:13:51 +0100

    Sidebar, text and code with highlighting

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var map = L.map('map');
+var map = L.map('map'),
+    sidebar = L.control.sidebar('sidebar', { position: 'left' }).addTo(map);
+
 
 L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png', {
 	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
 		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
 		'data uses <a href="http://opendatacommons.org/licenses/odbl/">ODbL</a> license'
 }).addTo(map);
+
+setTimeout(function () {
+    sidebar.show();
+}, 500);
 
 L.Routing.control({
     waypoints: [
