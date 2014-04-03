d37d7f0d04e45604d7077c253a3cae07e54c7b8b
Per Liedman <per@liedman.net>
2014-04-03 21:49:25 +0200

    Use whatever protocol the document was loaded with (OSRM now supports https)

--- src/L.Routing.OSRM.js
+++ src/L.Routing.OSRM.js
@@ -19,7 +19,7 @@
 	L.Routing.OSRM = L.Class.extend({
 		includes: L.Mixin.Events,
 		options: {
-			serviceUrl: 'http://router.project-osrm.org/viaroute',
+			serviceUrl: '//router.project-osrm.org/viaroute',
 			geometryPrecision: 6
 		},
 
