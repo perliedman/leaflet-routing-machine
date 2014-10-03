c1ed1e18a5f4e22b1fe6bb88dbf533f7d78d8191
Per Liedman <per@liedman.net>
2014-10-03 09:39:12 +0200

    feat(line) Changed style for better contrast against default OSM map

--- src/L.Routing.Line.js
+++ src/L.Routing.Line.js
@@ -8,9 +8,9 @@
 
 		options: {
 			styles: [
-				{color: 'black', opacity: 0.15, weight: 7},
-				{color: 'white', opacity: 0.8, weight: 4},
-				{color: 'orange', opacity: 1, weight: 2}
+				{color: 'black', opacity: 0.15, weight: 9},
+				{color: 'white', opacity: 0.8, weight: 6},
+				{color: 'red', opacity: 1, weight: 2}
 			],
 			missingRouteStyles: [
 				{color: 'black', opacity: 0.15, weight: 7},
