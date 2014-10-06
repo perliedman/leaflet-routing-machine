b5f6005de0ad2ceeb5fd5bdd42e8f32068ce87ae
perliedman <per@liedman.net>
2014-10-06 10:02:56 +0200

    fix(gh-pages): fix for options passing to plan

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var map = L.map('map'),
-    sidebar = L.control.sidebar('sidebar', { position: 'left' }).addTo(map);
+	sidebar = L.control.sidebar('sidebar', { position: 'left' }).addTo(map);
 
 
 L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png', {
@@ -9,18 +9,17 @@ L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 setTimeout(function () {
-    sidebar.show();
+	sidebar.show();
 }, 500);
 
 L.Routing.control({
-    waypoints: [
-        L.latLng(48.8588,2.3469),
-        L.latLng(52.3546,4.9039)
-    ],
-    geocoder: L.Control.Geocoder.nominatim(),
-    plan: L.Routing.plan(null, {
-        waypointIcon: function(i) {
-            return new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) });
-        }
-    })
-}).addTo(map);
+	plan: L.Routing.plan([
+			L.latLng(48.8588,2.3469),
+			L.latLng(52.3546,4.9039)
+		], {
+			waypointIcon: function(i) {
+				return new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) });
+			},
+			geocoder: L.Control.Geocoder.nominatim()
+		})
+	}).addTo(map);
