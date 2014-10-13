c4495a584983a27704d983d9e53eb604be21c6d9
Per Liedman <per@liedman.net>
2014-10-13 20:51:21 +0200

    Update map id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var map = L.map('map'),
 	sidebar = L.control.sidebar('sidebar', { position: 'left' }).addTo(map);
 
 
-L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png', {
+L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.jokgn3nn/{z}/{x}/{y}.png', {
 	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
 		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
 		'data uses <a href="http://opendatacommons.org/licenses/odbl/">ODbL</a> license'
