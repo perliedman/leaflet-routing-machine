344ff09c8bb94d4e42fa583286d95396d8227c65
perliedman <per.liedman@dotnetmentor.se>
2015-04-24 17:14:10 +0200

    Add GeoJSON export

---
+++ src/L.Routing.js
@@ -0,0 +1,64 @@
+(function() {
+	'use strict';
+
+	var L = require('leaflet');
+
+	L.Routing = L.Routing || {};
+
+	L.Routing.routeToGeoJson = function(route) {
+		var wpNames = [],
+			wpCoordinates = [],
+			i,
+			wp,
+			latLng;
+
+		for (i = 0; i < route.waypoints.length; i++) {
+			wp = route.waypoints[i];
+			latLng = L.latLng(wp.latLng);
+			wpNames.push(wp.name);
+			wpCoordinates.push([latLng.lng, latLng.lat]);
+		}
+
+		return {
+			type: 'FeatureCollection',
+			features: [
+				{
+					type: 'Feature',
+					properties: {
+						id: 'waypoints',
+						names: wpNames
+					},
+					geometry: {
+						type: 'MultiPoint',
+						coordinates: wpCoordinates
+					}
+				},
+				{
+					type: 'Feature',
+					properties: {
+						id: 'line',
+					},
+					geometry: L.Routing.routeToLineString(route)
+				}
+			]
+		};
+	};
+
+	L.Routing.routeToLineString = function(route) {
+		var lineCoordinates = [],
+			i,
+			latLng;
+
+		for (i = 0; i < route.coordinates.length; i++) {
+			latLng = L.latLng(route.coordinates[i]);
+			lineCoordinates.push([latLng.lng, latLng.lat]);
+		}
+
+		return {
+			type: 'LineString',
+			coordinates: lineCoordinates
+		};
+	};
+
+	module.exports = L.Routing;
+})();
