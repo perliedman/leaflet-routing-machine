d6ca9a2dcf45571b12384f870ef4bb32a58acc00
Per Liedman <per@liedman.net>
2015-09-08 21:00:08 +0200

    Enable showAlternatives on start page

--- index.js
+++ index.js
@@ -22,5 +22,13 @@ L.Routing.control({
 		routeWhileDragging: true
 	}),
 	routeWhileDragging: true,
-	routeDragTimeout: 250
+	routeDragTimeout: 250,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
 }).addTo(map);
