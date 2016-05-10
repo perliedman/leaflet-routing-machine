5f839f65870d3ccc115bf07d8c3cac784557a722
Per Liedman <per@liedman.net>
2016-05-10 11:17:28 +0200

    Forwardport 93eb33dbc72c37fef16eb95c1aede7556b5fd440

--- src/localization.js
+++ src/localization.js
@@ -438,5 +438,52 @@ module.exports = {
 			viaPlaceholder: 'Cez {viaNumber}',
 			endPlaceholder: 'Koniec'
 		}
+	},
+	'el': {
+		directions: {
+			N: 'βόρεια',
+			NE: 'βορειοανατολικά',
+			E: 'ανατολικά',
+			SE: 'νοτιοανατολικά',
+			S: 'νότια',
+			SW: 'νοτιοδυτικά',
+			W: 'δυτικά',
+			NW: 'βορειοδυτικά'
+		},
+		instructions: {
+			// instruction, postfix if the road is named
+			'Head':
+				['Κατευθυνθείτε {dir}', ' στην {road}'],
+			'Continue':
+				['Συνεχίστε {dir}', ' στην {road}'],
+			'SlightRight':
+				['Ελαφρώς δεξιά', ' στην {road}'],
+			'Right':
+				['Δεξιά', ' στην {road}'],
+			'SharpRight':
+				['Απότομη δεξιά στροφή', ' στην {road}'],
+			'TurnAround':
+				['Κάντε αναστροφή'],
+			'SharpLeft':
+				['Απότομη αριστερή στροφή', ' στην {road}'],
+			'Left':
+				['Αριστερά', ' στην {road}'],
+			'SlightLeft':
+				['Ελαφρώς αριστερά', ' στην {road}'],
+			'WaypointReached':
+				['Φτάσατε στο σημείο αναφοράς'],
+			'Roundabout':
+				['Ακολουθήστε την {exitStr} έξοδο στο κυκλικό κόμβο', ' στην {road}'],
+			'DestinationReached':
+				['Φτάσατε στον προορισμό σας'],
+		},
+		formatOrder: function(n) {
+			return n + 'º';
+		},
+		ui: {
+			startPlaceholder: 'Αφετηρία',
+			viaPlaceholder: 'μέσω {viaNumber}',
+			endPlaceholder: 'Προορισμός'
+		}
 	}
 };
