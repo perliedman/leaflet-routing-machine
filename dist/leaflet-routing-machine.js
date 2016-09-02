97bb02fbbcb7adfc7d0ccfced818ffdcec6e5abe
Per Liedman <per.liedman@dotnetmentor.se>
2016-09-02 15:01:11 +0200

    Dist files 3.1.1

--- dist/leaflet-routing-machine.js
+++ dist/leaflet-routing-machine.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.L || (g.L = {})).Routing = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),(f.L||(f.L={})).Routing=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function corslite(url, callback, cors) {
     var sent = false;
 
@@ -1606,7 +1606,8 @@ if (typeof module !== undefined) module.exports = polyline;
 				SharpRight: 'sharp right',
 				SlightLeft: 'slight left',
 				Left: 'left',
-				SharpLeft: 'sharp left'
+				SharpLeft: 'sharp left',
+				Uturn: 'Turn around'
 			},
 			instructions: {
 				// instruction, postfix if the road is named
@@ -1714,7 +1715,8 @@ if (typeof module !== undefined) module.exports = polyline;
 				SharpRight: 'skarpt höger',
 				SlightLeft: 'svagt vänster',
 				Left: 'vänster',
-				SharpLeft: 'skarpt vänster'
+				SharpLeft: 'skarpt vänster',
+				Uturn: 'Vänd'
 			},
 			instructions: {
 				// instruction, postfix if the road is named
