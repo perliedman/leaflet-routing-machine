c3fa040b47cb8ead1ba7bcabf3ae119b1d67678e
st1gok <st1gok@mail.ru>
2019-10-04 10:26:23 +0300

    Update control.js (#554)
    * Update control.js
    
    fix out of bounds error when a click occurs at the end of the line
    
    * Fix code style
    
    * Update control.js

--- src/control.js
+++ src/control.js
@@ -220,7 +220,9 @@
 
 		_hookEvents: function(l) {
 			l.on('linetouched', function(e) {
-				this._plan.dragNewWaypoint(e);
+				if (e.afterIndex < this.getWaypoints().length - 1) {
+					this._plan.dragNewWaypoint(e);
+				}
 			}, this);
 		},
 
