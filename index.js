1b20c2c20be081705290165823d9fa52a06174a4
curtisy <curtisy@mailbox.org>
2021-07-31 23:42:44 +0200

    Replace tiles and routing uri with OSM

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var map = L.map('map', { scrollWheelZoom: false }),
 		L.latLng(52.3546,4.9039)
 	];
 
-L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}{r}.png?access_token=' + LRM.apiToken, {
-	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
-		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Maps and routes from <a href="https://www.openstreetmap.org">OpenStreetMap</a>. ' +
 		'data uses <a href="http://opendatacommons.org/licenses/odbl/">ODbL</a> license'
 }).addTo(map);
 
 var control = L.Routing.control({
-		router: L.routing.mapbox(LRM.apiToken),
+		router: L.routing.osrmv1({
+			serviceUrl: 'https://routing.openstreetmap.de/routed-car/route/v1'
+		}),
 		plan: L.Routing.plan(waypoints, {
 			createMarker: function(i, wp) {
 				return L.marker(wp.latLng, {
