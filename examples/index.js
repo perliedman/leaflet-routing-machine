6d0a4ff58d7b6b23039148bb06085888ed263457
curtisy <curtisy@mailbox.org>
2021-09-04 14:40:24 +0200

    deps: Bump example leaflet version

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-var map = L.map('map');
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
